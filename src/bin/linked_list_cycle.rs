b01a2b2083fc2670de10f261cd22bdf99c4db0de
cabral-ramon <rrcm16@gmail.com>
2024-05-27 16:11:40 -0400

    this is cursed

--- src/bin/linked_list_cycle.rs
+++ src/bin/linked_list_cycle.rs
@@ -1,8 +1,25 @@
 extern crate rust_dsa;
+use std::{cell::RefCell};
+use std::rc::Rc;
 
-use rust_dsa::linked_list::Node;
+use rust_dsa::linked_list::{LinkedList, Node};
 
 fn main() {
-    let _n = Node::new(69);
-   println!("Hello noob");
+   let mut ll: LinkedList<i32> = LinkedList::new();
+   let mut node_3: Node<i32> = Node::new(3);
+   let node_2: Node<i32> = Node::new(2);
+   let node_1: Node<i32> = Node::new(1);
+   let node_1 = Rc::new(RefCell::new(node_1));
+   // create cycle
+   node_3.next = Some(node_1.clone());
+   ll.push_node(Rc::new(RefCell::new(node_3)));
+   ll.push_node(Rc::new(RefCell::new(node_2)));
+   ll.push_node(node_1.clone());
+
+   assert!(detect_linked_list_cycle(ll));
+}
+
+
+fn detect_linked_list_cycle<T>(list: LinkedList<T>) -> bool {
+    todo()!
 }
--- src/linked_list.rs
+++ src/linked_list.rs
@@ -1,84 +1,64 @@
+use std::cell::RefCell;
+use std::rc::Rc;
+
+type NodePtr<T> = Rc<RefCell<Node<T>>>;
+
 #[derive(Debug)]
 pub struct Node<T> {
-    value: T,
-    next: Option<Box<Node<T>>>
+    pub value: T,
+    pub next: Option<NodePtr<T>>,
 }
 
 impl<T> Node<T> {
     pub fn new(value: T) -> Self {
-        Self {
-            value,
-            next: None,
-        }
+        Self { value, next: None }
     }
 }
 
 #[derive(Debug)]
 pub struct LinkedList<T> {
-    head: Option<Box<Node<T>>>
+    pub head: Option<NodePtr<T>>,
 }
 
-impl<T> LinkedList<T> {
+impl<T: std::fmt::Debug> LinkedList<T> {
     pub fn new() -> Self {
-        Self {
-            head: None,
-        }
+        Self { head: None }
     }
 
     pub fn push(&mut self, value: T) {
         let mut node = Node::new(value);
         let curr_head = self.head.take();
         node.next = curr_head;
-        self.head = Some(Box::new(node));
+        self.head = Some(Rc::new(RefCell::new(node)));
+    }
+
+    pub fn push_node(&mut self, node_ptr: Rc<RefCell<Node<T>>>) {
+        let curr_head = self.head.take();
+        node_ptr.borrow_mut().next = curr_head;
+        self.head = Some(node_ptr);
     }
 
     pub fn pop(&mut self) -> Option<T> {
-        let mut curr_head = self.head.take()?;
-        let next = curr_head.next;
-        curr_head.next = None;
-        self.head = next;
-        Some(curr_head.value)
+        let curr_head = self.head.take()?;
+        let next = curr_head.borrow_mut().next.take();
+        // I don't think we need to do this since it gets dropped at the end of this scope
+        self.head = next.clone();
+        Some(
+            Rc::try_unwrap(curr_head)
+                .expect("There should have only been one pointer to this Node!")
+                .into_inner()
+                .value,
+        )
     }
 
-    pub fn peek(&self) -> Option<&T> {
-        self.head.as_ref().map(|node| &node.value)
+    pub fn peek(&self) -> Option<NodePtr<T>> {
+        // self.head.as_ref().map(|ptr| ptr.clone())
+        self.head.as_ref().cloned()
     }
 }
 
 impl<T> Default for LinkedList<T> {
     fn default() -> Self {
-        Self {
-            head: None,
-        }
-    }
-}
-
-#[cfg(test)]
-mod test {
-    use super::*;
-
-    #[test]
-    fn test_golden_path() {
-        let mut ll: LinkedList<i32> = LinkedList::new();
-        ll.push(1);
-        ll.push(2);
-        ll.push(3);
-
-        assert_eq!(ll.peek(), Some(&3));
-        assert_eq!(ll.pop(), Some(3));
-
-        assert_eq!(ll.peek(), Some(&2));
-        assert_eq!(ll.pop(), Some(2));
-
-        ll.push(42);
-
-        assert_eq!(ll.peek(), Some(&42));
-        assert_eq!(ll.pop(), Some(42));
-
-        assert_eq!(ll.peek(), Some(&1));
-        assert_eq!(ll.pop(), Some(1));
-
-        assert_eq!(ll.pop(), None);
-        assert_eq!(ll.peek(), None);
+        Self { head: None }
     }
 }
