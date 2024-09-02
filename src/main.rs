6a704bbdd380f1e754c2aa3a00907e82f33a93bf
cabral-ramon <rrcm16@gmail.com>
2024-09-02 13:06:20 -0400

    remove unused files

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,4 @@
-#[derive(PartialEq)]
-struct Foo<'a>(i32,&'a str);
-
+// see `examples/`
 fn main() {
     println!("Hello, world!");
-    let a = Box::new(Foo( 88, "Hello"));
-    let b = Box::new(Foo( 88, "Hello"));
-
-    if a == b {
-        println!("They are equal!");
-    }
 }
